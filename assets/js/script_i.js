fd06bd7e24cb8d886b2d5923f8f92a6f41014412
Michelle Tran <mptran0101@berkeley.edu>
2018-06-07 10:20:24 -0700

    overlay

--- assets/js/script_i.js
+++ assets/js/script_i.js
@@ -4,60 +4,54 @@ $(document).ready(function() {
     function() {
       $("#a-s").css("display", "none");
       $("#a-u").css("display", "flex");
-
       $("cursor").css("cursor", "pointer");
-
       $("#a-text").css("font-size", "36px");
+      $("#a-container").css("display", "inline-block")
   });
 
   $("#a").mouseout(
     function() {
       $("#a-s").css("display", "flex");
       $("#a-u").css("display", "none");
-
       $("cursor").css("cursor", "pointer");
-
       $("#a-text").css("font-size", "24px");
+      $("#a-container").css("display", "none")
   });
 
   $("#b").mouseover(
     function() {
       $("#b-s").css("display", "none");
       $("#b-u").css("display", "flex");
-
       $("cursor").css("cursor", "pointer");
-
       $("#b-text").css("font-size", "36px");
+      $("#b-container").css("display", "inline-block")
   });
 
   $("#b").mouseout(
     function() {
       $("#b-s").css("display", "flex");
       $("#b-u").css("display", "none");
-
       $("cursor").css("cursor", "pointer");
-
       $("#b-text").css("font-size", "24px");
+      $("#b-container").css("display", "none")
   });
 
   $("#c").mouseover(
     function() {
       $("#c-s").css("display", "none");
       $("#c-u").css("display", "flex");
-
       $("cursor").css("cursor", "pointer");
-
       $("#c-text").css("font-size", "36px");
+      $("#c-container").css("display", "inline-block")
   });
 
   $("#c").mouseout(
     function() {
       $("#c-s").css("display", "flex");
       $("#c-u").css("display", "none");
-
       $("cursor").css("cursor", "pointer");
-
       $("#c-text").css("font-size", "24px");
+      $("#c-container").css("display", "none")
   });
 
 
